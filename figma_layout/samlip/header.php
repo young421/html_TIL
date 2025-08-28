aac56ba4a458c91911f231b431c3792e5a25f44e
lucia042155 <lucia042155@gmail.com>
2025-08-28 18:16:11 +0900

    삼립 header.php연결 -> footer제작중

---
+++ figma_layout/samlip/header.php
@@ -0,0 +1,62 @@
+<header>
+            <h1><a href="./index.html"><img src="./images/logo_black.png" alt="SPC삼립"></a></h1>
+            <nav>
+                <ul class="gnb">
+                    <li><a href="./company.html">COMPANY</a></li>
+                    <li><a href="#">BRANDS</a></li>
+                    <li>
+                        <a href="#">IR</a>
+                        <ul class="lnb">
+                            <li>
+                                <a href="#">기업지배구조</a>
+                                <ul class="lnb_depth2">
+                                    <li><a href="#">이사회</a></li>
+                                    <li><a href="#">이사회 내 위원회</a></li>
+                                    <li><a href="#">주주총회</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">재무정보</a>
+                                <ul class="lnb_depth2">
+                                    <li><a href="#">재무상태표</a></li>
+                                    <li><a href="#">손익계산서</a></li>
+                                    <li><a href="#">현금흐름표</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">주식정보</a>
+                                <ul class="lnb_depth2">
+                                    <li><a href="#">공시</a></li>
+                                    <li><a href="#">실시간 주가정보</a></li>
+                                    <li><a href="#">배당현황</a></li>
+                                    <li><a href="#">공시정보관리규정</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">SNS</a></li>
+                    <li>
+                        <a href="#">RECRUIT</a>
+                        <!-- 마우스로 보이는 영역 -->
+                        <ul class="lnb">
+                            <li><a href="#">인사제도</a></li>
+                            <li><a href="#">입사지원</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">STORE</a></li>
+                </ul>
+            </nav>
+            <ul class="right_menu">
+                <li><a href="#">CUSTOMER</a></li>
+                <li>
+                    <a href="#"><img src="./images/lang_kr.png" alt="한국어보기"></a>
+                    <!-- 마우스 올릴 시 보이는 영역 -->
+                    <div class="lang_select">
+                        <a href="#" class="kr">KR</a>
+                        <a href="#" class="en">EN</a>
+                        <a href="#" class="cn">CN</a>
+                    </div>
+                </li>
+                <li><a href="#"><img src="./images/search.png" alt="검색하기"></a></li>
+            </ul>
+        </header>
